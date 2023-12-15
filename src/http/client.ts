d433320195a12b0ce7ec3796ddb0a97fad85e93c
何乾 <182359041@qq.com>
2023-12-15 18:40:19 +0800

    初始化

---
+++ src/http/client.ts
@@ -0,0 +1,33 @@
+import {Client, Result} from "../hbuf/server"
+import {Data} from "../hbuf/data";
+
+type RequestInterceptor = Function
+
+export class HttpClientJson implements Client {
+    protected baseUrl: string;
+
+    invoke<T>(serverName: string, serverId: number, name: string, id: number, req: Data, fromJson: (json: {}) => T, fromData: (json: BinaryData) => T): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            let request = new XMLHttpRequest()
+            request.open("POST", this.baseUrl + "/" + serverName + name, false)
+            request.setRequestHeader('Content-Type', 'application/json');
+
+            request.send(JSON.stringify(req))
+
+            if (request.status != 200) {
+                reject(request.responseText)
+            }
+
+            let result = JSON.parse(request.responseText) as Result
+            if (0 != result.code) {
+                reject(result)
+            }
+            resolve(fromJson(result.data))
+        });
+    }
+
+    constructor(baseUrl: string) {
+        this.baseUrl = baseUrl;
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import {Data} from "./hbuf/data";
+import {Context, Result, Client, ServerClient} from "./hbuf/server";
+import {HttpClientJson} from "./http/client";
+
+export {
+    Data,
+    Context,
+    Result,
+    Client,
+    ServerClient,
+    HttpClientJson
+}
+
